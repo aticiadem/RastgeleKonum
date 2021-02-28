b5ef71968dbf3589181e0e4b50f9cf3a7cefe451
Adem Atici <adematicis41@gmail.com>
2021-02-28 17:11:32 +0300

    SplashScreen tamamlandi.

---
+++ app/src/main/java/com/adematici/rastgelekonum/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.adematici.rastgelekonum
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity,MainActivity::class.java)
+            startActivity(intent)
+        },800)
+    }
+
+}
